64177fc66804c640ba11b5950b112932c1620d0c
Kevin K <ktkinsey37@gmail.com>
2022-03-06 22:05:28 -0700

    items and events

--- models/events.js
+++ models/events.js
@@ -49,140 +49,19 @@ class Event {
    * Returns list of animal objects [{ name, species, weight, birthDate, sex, colorationPattern, primaryColor, secondaryColor, price, forSale }, ...]
    * */
 
-  static async findAll(searchFilters = {}) {
-    let query = `SELECT id,
-                        name,
-                        species,
-                        weight,
-                        birth_date,
-                        sex,
-                        coloration_pattern AS "colorationPattern",
-                        primary_color AS "primaryColor",
-                        secondary_color AS "secondaryColor",
-                        price,
-                        for_sale AS "forSale",
-                        img_url AS "imgUrl"
-                 FROM animals`;
-    let whereExpressions = [];
-    let queryValues = [];
+  static async getAll() {
+    let query = `SELECT *
+                 FROM events
+                 ORDER BY date DESC;`;
 
-    const {
-      name = undefined,
-      species = undefined,
-      minWeight = undefined,
-      maxWeight = undefined,
-      // minAge=undefined, maxAge=undefined,
-      sex = undefined,
-      colorationPattern = undefined,
-      primaryColor = undefined,
-      secondaryColor = undefined,
-      maxPrice = undefined,
-      minPrice = undefined,
-      forSale = undefined,
-    } = searchFilters;
-
-    // NEED TO ADD FILTER BY, POSSIBLY BY PASSING THROUGH AN OBJ WITH FITLERBY AND ASC/DESC BOOL?
-
-    if (minWeight >= maxWeight) {
-      throw new BadRequestError(
-        "Min weight cannot be greater than or equal to max"
-      );
-    }
-
-    // if (minAge >= maxAge) {
-    //   throw new BadRequestError("Min age cannot be greater than or equal to max");
-    // }
-
-    if (minPrice >= maxPrice) {
-      throw new BadRequestError(
-        "Min price cannot be greater than or equal to max"
-      );
-    }
-
-    // For each possible search term, add to whereExpressions and queryValues so
-    // we can generate the right SQL
-
-    if (name !== undefined) {
-      queryValues.push(`%${name}%`);
-      whereExpressions.push(`name ILIKE $${queryValues.length}`);
-    }
-
-    if (species !== undefined) {
-      queryValues.push(`%${species}%`);
-      whereExpressions.push(`species ILIKE $${queryValues.length}`);
-    }
-
-    if (minWeight !== undefined) {
-      queryValues.push(`%${minWeight}%`);
-      whereExpressions.push(`weight < $${queryValues.length}`);
-    }
-
-    if (maxWeight !== undefined) {
-      queryValues.push(`%${maxWeight}%`);
-      whereExpressions.push(`weight > $${queryValues.length}`);
-    }
-
-    // if (minAge !== undefined) {
-    //   queryValues.push(`%${minAge}%`);
-    //   whereExpressions.push(`age > $${queryValues.length}`);
-    // }
-
-    // if (maxAge !== undefined) {
-    //   queryValues.push(`%${maxAge}%`);
-    //   whereExpressions.push(`age < $${queryValues.length}`);
-    // }
-
-    if (minPrice !== undefined) {
-      queryValues.push(`%${minPrice}%`);
-      whereExpressions.push(`price > $${queryValues.length}`);
-    }
-
-    if (maxPrice !== undefined) {
-      queryValues.push(`%${maxPrice}%`);
-      whereExpressions.push(`price < $${queryValues.length}`);
-    }
-
-    if (sex !== undefined) {
-      queryValues.push(`%${sex}%`);
-      whereExpressions.push(`sex ILIKE $${queryValues.length}`);
-    }
-
-    if (colorationPattern !== undefined) {
-      queryValues.push(`%${colorationPattern}%`);
-      whereExpressions.push(`coloration_pattern ILIKE $${queryValues.length}`);
-    }
-
-    if (primaryColor !== undefined) {
-      queryValues.push(`%${primaryColor}%`);
-      whereExpressions.push(`primary_color ILIKE $${queryValues.length}`);
-    }
-
-    if (secondaryColor !== undefined) {
-      queryValues.push(`%${secondaryColor}%`);
-      whereExpressions.push(`secondary_color ILIKE $${queryValues.length}`);
-    }
-
-    if (forSale !== undefined) {
-      queryValues.push(`%${forSale}%`);
-      whereExpressions.push(`for_sale ILIKE $${queryValues.length}`);
-    }
-
-    if (whereExpressions.length > 0) {
-      query += " WHERE " + whereExpressions.join(" AND ");
-    }
-
-    // Finalize query and return results
-
-    query += " ORDER BY name;";
-    //console.log(query, "****this is query", queryValues, "suqeryvalues")
-    const animalsRes = await db.query(query, queryValues);
+    const eventsRes = await db.query(query);
     //console.log(animalsRes, "animalsRes")
-    return animalsRes.rows;
+    return eventsRes.rows;
   }
 
-  /** Given an animal id, return data about that particular animal.
+  /** Given an event id, return data about that particular event.
    *
-   * Returns { id, name, species, weight, age, sex, colorationPattern, primaryColor, secondaryColor, price, forSale, [...parents], [...children] }
+   * Returns { id, title, date, description}
    *
    * Calls findParents and findChildren below
    *
@@ -190,72 +69,23 @@ class Event {
    **/
 
   static async get(id) {
-    const animalRes = await db.query(
+    const eventRes = await db.query(
       `SELECT id,
-                  name,
-                  species,
-                  weight,
-                  birth_date,
-                  sex,
-                  coloration_pattern AS "colorationPattern",
-                  primary_color AS "primaryColor",
-                  secondary_color AS "secondaryColor",
-                  price,
-                  for_sale AS "forSale",
-                  img_url AS "imgUrl"
-           FROM animals
+              title,
+              date,
+              description
+           FROM event
            WHERE id = $1`,
       [id]
     );
 
-    const animal = animalRes.rows[0];
+    const event = eventRes.rows[0];
 
-    if (!animal) throw new NotFoundError(`No such animal with id: ${id}`);
+    if (!event) throw new NotFoundError(`No such event with id: ${id}`);
 
     // Maybe add parentage in herE?
 
-    return animal;
-  }
-
-  static async findParents(id) {
-    const relationshipRes = await db.query(
-      `SELECT parent_id AS "parentId"
-       FROM parent_children
-       WHERE child_id=$1`,
-      [id]
-    );
-
-    const parentIds = relationshipRes.rows[0];
-
-    if (!parentIds) throw new NotFoundError(`No parents`);
-
-    return parentIds;
-  }
-
-  static async findChildren(id) {
-    const relationshipRes = await db.query(
-      `SELECT child_id AS "childId"
-       FROM parent_children
-       WHERE parent_id=$1`,
-      [id]
-    );
-
-    const childrenIds = relationshipRes.rows[0];
-
-    if (!childrenIds) throw new NotFoundError(`No parents`);
-
-    return childrenIds;
-  }
-
-  // Adds a parent-child relationship by adding their respective IDs into table
-
-  static async addParentage(parentId, childId) {
-    const parentageRes = await db.query(
-      `INSERT INTO parent_children
-      (parent_id, child_id)
-      VALUES ($1, $2)`,
-      [parentId, childId]
-    );
+    return event;
   }
 
   /** Update company data with `data`.
@@ -271,30 +101,23 @@ class Event {
    */
 
   static async update(id, data) {
-    const { setCols, values } = sqlForPartialUpdate(data, {
-      birthDate: "birth_date",
-      colorationPattern: "coloration_pattern",
-      primaryColor: "primary_color",
-      secondaryColor: "secondary_color",
-      forSale: "for_sale",
-      imgUrl: "img_url",
-    });
+    const { setCols, values } = sqlForPartialUpdate(data, {});
     const idVarIdx = "$" + (values.length + 1);
 
-    const querySql = `UPDATE animals 
+    const querySql = `UPDATE events 
                       SET ${setCols} 
                       WHERE id = ${id} 
                       RETURNING title, date, description`;
     const result = await db.query(querySql, [...values, id]);
-    const animal = result.rows[0];
+    const event = result.rows[0];
 
-    if (!animal) throw new NotFoundError(`No such animal with id: ${id}`);
+    if (!event) throw new NotFoundError(`No such event with id: ${id}`);
 
     console.log(
-      animal,
-      "this is animal right before return in animal.js update"
+      event,
+      "this is event right before return in event.js update"
     );
-    return animal;
+    return event;
   }
 
   /** Delete given company from database; returns undefined.
---
+++ routes/events.js
@@ -0,0 +1,145 @@
+"use strict";
+
+/** Routes for companies. */
+
+const jsonschema = require("jsonschema");
+const express = require("express");
+
+const { BadRequestError } = require("../expressError");
+const { ensureAdmin } = require("../middleware/auth");
+const Event = require("../models/events");
+
+const eventUpdateSchema = require("../schemas/eventUpdate.json");
+// const eventSearchSchema = require("../schemas/eventSearch.json");
+const eventNewSchema = require("../schemas/eventNew.json");
+
+const router = new express.Router();
+
+/** POST / { company } =>  { company }
+ *
+ * company should be { handle, name, description, numEmployees, logoUrl }
+ *
+ * Returns { handle, name, description, numEmployees, logoUrl }
+ *
+ * Authorization required: admin
+ */
+// const multer = require("multer");
+
+// let storage = multer.diskStorage({
+//   destination: "./photos/",
+//   filename: function (req, file, cb) {
+//     //req.body is empty...
+//     //How could I get the new_file_name property sent from client here?
+//     cb(null, Date.now() + file.originalname.replace(/ /g, ""));
+//   },
+// });
+
+// let upload = multer({ storage: storage });
+
+router.post(
+  "/",
+  ensureAdmin,
+  async (req, res, next) => {
+    try {
+      const validator = jsonschema.validate(req.body, eventNewSchema);
+      if (!validator.valid) {
+        const errs = validator.errors.map((e) => e.stack);
+        throw new BadRequestError(errs);
+      }
+
+      const event = await Event.create(req.body);
+      return res.status(201).json({ event });
+    } catch (err) {
+      return next(err);
+    }
+  }
+);
+
+/** GET /  =>
+ *   { companies: [ { handle, name, description, numEmployees, logoUrl }, ...] }
+ *
+ * Can filter on provided search filters:
+ * - minEmployees
+ * - maxEmployees
+ * - nameLike (will find case-insensitive, partial matches)
+ *
+ * Authorization required: none
+ */
+
+router.get("/", async function (req, res, next) {
+  const q = req.query;
+  // arrive as strings from querystring, but we want as ints
+
+  try {
+    //   Don't need to validate because it's just getting all, searches can be added later
+    // const validator = jsonschema.validate(q, animalSearchSchema);
+    // if (!validator.valid) {
+    //   const errs = validator.errors.map((e) => e.stack);
+    //   throw new BadRequestError(errs);
+    // }
+
+    const events = await Event.getAll();
+    return res.json({ events });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET /[id]  =>  { animal }
+ *
+ *  Animal is { handle, name, description, numEmployees, logoUrl, jobs }
+ *   where jobs is [{ id, title, salary, equity }, ...]
+ *
+ * Authorization required: none
+ */
+
+router.get("/:id", async function (req, res, next) {
+  try {
+    const event = await Event.get(req.params.id);
+    return res.json({ event });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** PATCH /[handle] { fld1, fld2, ... } => { company }
+ *
+ * Patches company data.
+ *
+ * fields can be: { name, description, numEmployees, logo_url }
+ *
+ * Returns { handle, name, description, numEmployees, logo_url }
+ *
+ * Authorization required: admin
+ */
+
+router.patch("/:id", ensureAdmin, async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, eventUpdateSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const event = await Event.update(req.params.id, req.body);
+    return res.json({ event });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** DELETE /[handle]  =>  { deleted: handle }
+ *
+ * Authorization: admin
+ */
+
+router.delete("/:id", ensureAdmin, async function (req, res, next) {
+  try {
+    await Event.remove(req.params.id);
+    return res.json({ deleted: req.params.id });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
